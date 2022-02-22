0b7b5d04134017adc7c9ca27b2e7b1e04b36854f
redsIng <80847977+redsIng@users.noreply.github.com>
2022-02-22 23:19:41 +0100

    up

---
+++ PorveLatexEtc/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String str = "Ciao";
+        if (!str.isEmpty()) {
+            System.out.println("String Non vuota");
+        }
+
+    }
+}
