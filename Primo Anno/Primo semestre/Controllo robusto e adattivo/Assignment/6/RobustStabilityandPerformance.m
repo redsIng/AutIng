fc0798d5a0f24f5731ac763a0cdaf0ab625b9ae8
redsIng <80847977+redsIng@users.noreply.github.com>
2022-06-09 18:48:21 +0200

    up

--- Primo Anno/Primo semestre/Controllo robusto e adattivo/Assignment/6/RobustStabilityandPerformance.m
+++ Primo Anno/Primo semestre/Controllo robusto e adattivo/Assignment/6/RobustStabilityandPerformance.m
@@ -3,31 +3,86 @@
 clc
 clear
 close all
-%%
-k=[1 1.5 5/3 2.5 3];
-n = length(k);
 
-%% Valutazione Stabilità Robusta e Prestazioni robuste
+%% Stabilità Nominale
+ks=[0.1 0.5 1 2 10];
+ns = length(ks);
 lgdStr = compose('%g',k);
-hold on
-for i =1: n
-    W2T=tf([0 2*k(i)],[1 k(i)+9 10*k(i)-10]);
-    W1S=tf([1 -1],[1 k(i) k(i)-1]);
-    figure(2)
-    bode(W2T);
-    legend(lgdStr)
-    hold on
-    
-    figure(3)
-    nyquist(W2T)
-    hold on
 
-    figure(4)
-    bode(W1S);
-    legend(lgdStr)
+for i = 1:ns
+    S = tf([ks(i)],[1 -1/ks(i)]);
+
+    figure(1)
+    clf
+    rlocusplot(S);grid on
     hold on
-%     pause()
+    legend(lgdStr{1,1:i})
+
+
 end
+
+%%
+k=[1 1.5 5/3 2.5];
+n = length(k);
+P = tf(1,[1 -1]);
+
+%% Stabiliytà Robuste
+subplot(4,2,1)
+W2T=tf([1 -1],[1 k(1) k(1)-1]);
+h=bodeplot(W2T);
+setoptions(h,'PhaseVisible','off')
+title('W2T-k=1')
+subplot(4,2,2)
+nyquist(k(1)*P)
+
+
+subplot(4,2,3)
+W2T=tf([0 2*k(2)],[1 k(2)+9 10*k(2)-10]);
+h=bodeplot(W2T);
+setoptions(h,'PhaseVisible','off')
+title('W2T-k=1.5')
+subplot(4,2,4)
+nyquist(k(2)*P)
+
+
+subplot(4,2,5)
+W2T=tf([0 2*k(3)],[1 k(3)+9 10*k(3)-10]);
+h=bodeplot(W2T);
+setoptions(h,'PhaseVisible','off')
+title('W2T-k=5/3')
+subplot(4,2,6)
+nyquist(k(3)*P)
+
+
+subplot(4,2,7)
+W2T=tf([0 2*k(4)],[1 k(4)+9 10*k(4)-10]);
+h=bodeplot(W2T);
+setoptions(h,'PhaseVisible','off')
+title('W2T-k=2.5')
+subplot(4,2,8)
+nyquist(k(4)*P)
+%% Prestazioni Robuste
+
+subplot(4,1,1)
+W1S=tf([1 -1],[1 k(1) k(1)-1]);h=bodeplot(W1S);
+setoptions(h,'PhaseVisible','off')
+title('W1S-k=1')
+
+
+subplot(4,1,2)
+W1S=tf([1 -1],[1 k(2) k(2)-1]);h=bodeplot(W1S);
+setoptions(h,'PhaseVisible','off')
+title('W1S-k=1.5')
+
+subplot(4,1,3)
+W1S=tf([1 -1],[1 k(2) k(2)-1]);h=bodeplot(W1S);
+setoptions(h,'PhaseVisible','off')
+title('W1S-k=5/3')
+
+subplot(4,1,4)
+W1S=tf([1 -1],[1 k(3) k(3)-1]);h=bodeplot(W1S);
+setoptions(h,'PhaseVisible','off')
+title('W1S-k=2.5')
 %% Prestazioni robuste: alpha min
 syms k alpha(k)
 figure(5)
